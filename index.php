50fdc9e788cac704584463256f82d72cf28c38b4
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-02 15:51:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $str = '1 + 1';
-$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
-				$a = expolde(" ",$text);
-				$text = $a[0]+$a[3];
-echo $text;
+//$int = filter_var($str, FILTER_SANITIZE_NUMBER_INT);
+				//$a = expolde(" ",$text);
+				//$text = $a[0]+$a[3];
+echo $str;
 
