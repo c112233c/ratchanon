8e95c87f28111741337699d1d0d9ae03135706c6
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-05 15:38:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 			
 			
 			//if($text == "test"){
-			$con = @mysqli_connect('db4free.net:3307', 'c112233v', 'C112233v', 'linenavy');
+			$con = @new mysqli_connect('db4free.net:3307', 'c112233v', 'C112233v', 'linenavy');
 			if (!$con) {
 			   // echo "Error: " . mysqli_connect_error();
 				$text = "error";
