c53d97914d5ec9d8837fddcd00128298302713e1
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-05 15:35:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,11 +17,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			
 			
-			if($text == "test"){
+			//if($text == "test"){
 			$con = @mysqli_connect('db4free.net:3307', 'c112233v', 'C112233v', 'linenavy');
 			if (!$con) {
 			   // echo "Error: " . mysqli_connect_error();
-				exit();
+				$text = "error";
+				//exit();
 			}
 			
 			// Some Query
@@ -29,11 +30,11 @@ if (!is_null($events['events'])) {
 			$query 	= mysqli_query($con, $sql);
 			while ($row = mysqli_fetch_array($query))
 			{
-				$text = $row['Name']."  ".$row['SName]."\n";
+				$text = $text." ".$row['Name']."  ".$row['SName]." ";
 			}
 			
 			mysqli_close ($con);
-			}
+			//}
 
 			
 			//
