db8177c1ed20f21c0072879353856fdb5e59f7a5
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-06 09:51:20 +0700

    Update bot.php

--- php/bot.php
+++ php/bot.php
@@ -1,3 +1,21 @@
-<?
+<?php
 
-  echo "asd";
+// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
+
+include ('php/line-bot.php');
+
+$channelSecret = 'c2c62e7d395681a2eae042937aded03f';
+$access_token  = '50lIQBfp4lMHXijFOx4+AiCk1ZXAiu7GC6lZaBjsxaP2v8TQjt3WERrDzy4uOedRmNtxt+8nuT8oyV28keCfzYd9BzY25+7wamtalC5xFzxwQeFfB0QIblC6FBSYFFRPjTBZAO8a42/efYM+v/VP/AdB04t89/1O/w1cDnyilFU=';
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+$bot->sendMessageNew('Udc14115eb6c67983f1c7876c3007933a', 'Hello World !!');
+
+if ($bot->isSuccess()) {
+	echo 'Succeeded!';
+	exit();
+}
+
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
