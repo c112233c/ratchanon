77839b8dc5b2b7d5c59363544ea06ab40e9ef236
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-06 10:09:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,12 +7,5 @@ $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
 		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
+		echo "aaa";
 }
