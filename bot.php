c2647029b94c26f54d5a64cd4093db31f71440d3
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-06 11:03:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,30 +1,20 @@
 <?php
 $access_token = '50lIQBfp4lMHXijFOx4+AiCk1ZXAiu7GC6lZaBjsxaP2v8TQjt3WERrDzy4uOedRmNtxt+8nuT8oyV28keCfzYd9BzY25+7wamtalC5xFzxwQeFfB0QIblC6FBSYFFRPjTBZAO8a42/efYM+v/VP/AdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
+
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
 			
-			
-			
+			$text = "sssss";
+			$user = "Udc14115eb6c67983f1c7876c3007933a";
+
+			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'replyToken' => $replyToken,
+				'to'  => $user,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
