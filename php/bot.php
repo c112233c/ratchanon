e745a3a329d1762fbbf8cbb6323a7ca9f2bcc7fd
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-06 09:54:15 +0700

    Update bot.php

--- php/bot.php
+++ php/bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-02.php
 
-include ('php/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = 'c2c62e7d395681a2eae042937aded03f';
 $access_token  = '50lIQBfp4lMHXijFOx4+AiCk1ZXAiu7GC6lZaBjsxaP2v8TQjt3WERrDzy4uOedRmNtxt+8nuT8oyV28keCfzYd9BzY25+7wamtalC5xFzxwQeFfB0QIblC6FBSYFFRPjTBZAO8a42/efYM+v/VP/AdB04t89/1O/w1cDnyilFU=';
