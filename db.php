28698407523ce283c57286cffada4f7ccbcc94f8
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-08 09:37:52 +0700

    Update db.php

--- db.php
+++ db.php
@@ -7,7 +7,15 @@ if (!$con) {
 }
 
 // Some Query
-$sql 	= 'SELECT * FROM test';
+$sql 	= 'SELECT (
+		CASE 
+		WHEN CHECKTIME BETWEEN ('2018-02-06 08:00:00') and ('2018-02-06 08:30:00') then 'Present'
+		WHEN CHECKTIME BETWEEN ('2018-02-06 08:30:00') and ('2018-02-06 09:30:00') then 'Late'
+		ELSE '8' END) as Type
+		,count(USERID) as usercount
+		FROM CHECKINOUT
+		where CHECKTIME like '2018-02-06%'
+		group by Type';
 $query 	= mysqli_query($con, $sql);
 while ($row = mysqli_fetch_array($query))
 {
