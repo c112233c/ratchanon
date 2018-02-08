dae40746bce84891c5598fb839337b883e085735
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-08 10:32:33 +0700

    Update db.php

--- db.php
+++ db.php
@@ -9,10 +9,10 @@ if (!$con) {
 // Some Query
 $sql 	= 'SELECT * FROM test';
 			$query 	= mysqli_query($con, $sql);
-			while ($row = mysqli_fetch_array($query))
-			{
-				$text = json_encode($row);
-			}
+			$row = mysqli_fetch_array($query);
+			
+				$text = $row[0];
+			
 echo $text;
 //echo $text;
 echo 'Connected to MySQL';
