e193734544f4d05d797e49784525b21c64f64469
c112233c <36060861+c112233c@users.noreply.github.com>
2018-02-12 14:56:27 +0700

    Update push.php

--- push.php
+++ push.php
@@ -2,9 +2,31 @@
  
 $access_token = '50lIQBfp4lMHXijFOx4+AiCk1ZXAiu7GC6lZaBjsxaP2v8TQjt3WERrDzy4uOedRmNtxt+8nuT8oyV28keCfzYd9BzY25+7wamtalC5xFzxwQeFfB0QIblC6FBSYFFRPjTBZAO8a42/efYM+v/VP/AdB04t89/1O/w1cDnyilFU=';
 
+//Connect DB
+$con = @mysqli_connect('db4free.net:3307', 'c112233v', 'C112233v', 'linenavy');
+if (!$con) {
+    echo "Error: " . mysqli_connect_error();
+	exit();
+}
+// Some Query
+$sql 	= "SELECT ( CASE 
+WHEN CHECKTIME BETWEEN '2018-02-06 07:30:00' AND '2018-02-06 08:30:00' THEN '1'
+WHEN CHECKTIME BETWEEN '2018-02-06 08:30:00' AND '2018-02-06 09:30:00' THEN '2'
+ELSE '8' END) AS Type , COUNT(USERID)
+FROM CHECKINOUT
+WHERE USERID BETWEEN 155 AND 187
+GROUP BY Type
+ORDER BY Type ";
+$query 	= mysqli_query($con, $sql);
+$row = mysqli_fetch_array($query);
+$present = $row[1];
+$row = mysqli_fetch_array($query);
+$late = $row[1];
+mysqli_close ($con);
+$sum = 32-$present-$late;
 
-  $text = "ทดสอบระบบ Pushhhhhh";
-
+//Build Text
+$text = "ยอดเข้าเรียนของนักเรียนหลักสูตรอาชีพเพื่อเลื่อนฐานะชั้นจ่าเอก มาเรียน ".$present." นาย มาสาย ".$late." นาย ขาด ".$sum." นาย";
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
